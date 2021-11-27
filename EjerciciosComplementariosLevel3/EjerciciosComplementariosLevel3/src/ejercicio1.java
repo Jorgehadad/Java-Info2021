5e70a6616deededbfed873e99392469900d8f094
Jorgehadad <jorgemenegaz@gmail.com>
2021-11-27 16:55:44 -0300

    corregido lo que puso el profe que debía hacerse con streams

--- EjerciciosComplementariosLevel3/EjerciciosComplementariosLevel3/src/ejercicio1.java
+++ EjerciciosComplementariosLevel3/EjerciciosComplementariosLevel3/src/ejercicio1.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ejercicio1 {
     public static void main(String[] args) throws Exception {
@@ -7,14 +8,10 @@ public class ejercicio1 {
         palabras.add(null);
         palabras.add("Informatorio");
         palabras.add("");
-        //Dada una lista que contiene valores de tipo String, 
-        //se deberá filtrar todos los valores que NO sean null o vacío ("")
-        List<String> palabrasFiltradas = new ArrayList<>();
-        for (String palabra : palabras) {
-            if (palabra != null && !palabra.isEmpty()) {
-                palabrasFiltradas.add(palabra);
-            }
-        }
-        System.out.println(palabrasFiltradas);
+        //Dada una lista que contiene valores de tipo String, se deberá filtrar con STREAM todos los valores que NO sean null o vacío ("")
+        //y mostrarlos por pantalla.
+        List<String> palabrasResultado = palabras.stream().filter(p -> p != null && !p.isEmpty()).collect(Collectors.toList());
+        
+        System.out.println(palabrasResultado);
     }
 }
--- EjerciciosComplementariosLevel3/EjerciciosComplementariosLevel3/src/ejercicio5.java
+++ EjerciciosComplementariosLevel3/EjerciciosComplementariosLevel3/src/ejercicio5.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ejercicio5 {
     public static void main(String[] args) throws Exception {
@@ -10,14 +11,12 @@ public class ejercicio5 {
         alumnos.add(new Alumno("Bart", "Simpson", LocalDate.now().minusYears(30)));
         alumnos.add(new Alumno("Lisa", "Simpson", LocalDate.now().minusYears(30)));
         alumnos.add(new Alumno("Maggie", "Simpson", LocalDate.now().minusYears(30)));
-        alumnos.add(new Alumno("Marge", "Simpson", LocalDate.now().minusYears(30)));
 
-//Se desea generar un Map<String, Integer> donde la clave de Map será el apellido concatenado con el nombre 
-//(con separador de espacio en blanco) y el value la edad del alumno.
-        Map<String, Integer> edades = new HashMap<>();
-        for (Alumno alumno : alumnos) {
-            edades.put(alumno.getApellido() + " " + alumno.getNombre(), alumno.getEdad());
-        }
+
+//Se desea generar con STREAM y con Map<String, Integer> donde la clave de Map será el apellido concatenado con el nombre (con separador de espacio en blanco) y el value la edad del alumno.
+        Map<String, Integer> edades = alumnos.stream()
+                .collect(Collectors.toMap(a -> a.getApellido() + " " + a.getNombre(), a -> a.getEdad()));
         System.out.println(edades);
     }
+
 }
