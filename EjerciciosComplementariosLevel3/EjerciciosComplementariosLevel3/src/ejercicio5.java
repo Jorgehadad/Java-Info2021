cd20ca6bdc07bd2c87b8fd74a6b66497c238d75b
Jorgehadad <jorgemenegaz@gmail.com>
2021-11-29 10:40:58 -0300

    RE FIXING

--- EjerciciosComplementariosLevel3/EjerciciosComplementariosLevel3/src/ejercicio5.java
+++ EjerciciosComplementariosLevel3/EjerciciosComplementariosLevel3/src/ejercicio5.java
@@ -6,11 +6,11 @@ public class ejercicio5 {
     public static void main(String[] args) throws Exception {
 
         List<Alumno> alumnos = new ArrayList<>();
-        alumnos.add(new Alumno("Homero", "Simpson", LocalDate.now().minusYears(30)));
-        alumnos.add(new Alumno("Marge", "Simpson", LocalDate.now().minusYears(30)));
-        alumnos.add(new Alumno("Bart", "Simpson", LocalDate.now().minusYears(30)));
-        alumnos.add(new Alumno("Lisa", "Simpson", LocalDate.now().minusYears(30)));
-        alumnos.add(new Alumno("Maggie", "Simpson", LocalDate.now().minusYears(30)));
+        alumnos.add(new Alumno("Homero", "Simpson", LocalDate.parse("2017-03-10").minusYears(30)));
+        alumnos.add(new Alumno("Marge", "Simpson", LocalDate.parse("2017-03-10").minusYears(30)));
+        alumnos.add(new Alumno("Bart", "Simpson", LocalDate.parse("2017-03-10").minusYears(30)));
+        alumnos.add(new Alumno("Lisa", "Simpson", LocalDate.parse("2017-03-10").minusYears(30)));
+        alumnos.add(new Alumno("Maggie", "Simpson", LocalDate.parse("2017-03-10").minusYears(30)));
 
 
 //Se desea generar con STREAM y con Map<String, Integer> donde la clave de Map será el apellido concatenado con el nombre (con separador de espacio en blanco) y el value la edad del alumno.
